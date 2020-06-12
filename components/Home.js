de603c7bffbbe5761b69e21d6f6c2012edda1916
ArthurBrunet <43806925+ArthurBrunet@users.noreply.github.com>
2020-06-12 16:17:38 +0200

    premier composant / test des props

---
+++ components/Home.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import {Text, TextInput, View, Button} from 'react-native';
+
+export default class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        valueInput: props.valueInput
+    }
+  }
+
+  render() {
+    return (
+      <View>
+        <TextInput value={this.state.valueInput} onChangeText={(text) => this.setState({valueInput : text})}/>
+        <Button onPress={() => {console.log(this.state.valueInput)}} title="Le Button test" />
+        <Text>Hello, I am test component HOME !</Text>
+      </View>
+    );
+  }
+}
