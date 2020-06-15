2443724f1978616fe6245b98025a59b3f106a568
ArthurBrunet <Arthur.brunet76@gmail.com>
2020-06-15 11:28:55 +0200

    test speedometer + ajout npm redux

--- components/Home.js
+++ components/Home.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import {Text, TextInput, View, Button} from 'react-native';
+import RNSpeedometer from 'react-native-speedometer'
 
 export default class Home extends Component {
   constructor(props) {
@@ -15,6 +16,7 @@ export default class Home extends Component {
         <TextInput value={this.state.valueInput} onChangeText={(text) => this.setState({valueInput : text})}/>
         <Button onPress={() => {console.log(this.state.valueInput)}} title="Le Button test" />
         <Text>Hello, I am test component HOME !</Text>
+          <RNSpeedometer value={this.state.valueInput} size={300}/>
       </View>
     );
   }
